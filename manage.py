e8d6dde7de7920212677049ea8eb79d7a96d42e7
Christopher Golitschek <informatik@golitschek.de>
2022-03-07 15:50:46 +0000

    Finish omdb api

--- manage.py
+++ manage.py
@@ -2,13 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'course4_proj.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
